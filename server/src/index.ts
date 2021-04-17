f948b2b57e67c8adf8973c06b31c37d4a1d73a67
Mark Ambro <wowmarkambro@gmail.com>
2021-04-16 22:55:29 -0500

    fixed app start files

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,37 +1,8 @@
-import cors from "cors";
 import * as dotenv from "dotenv";
-import express, { Express } from "express";
-import helmet from "helmet";
-import morgan from "morgan";
-import connectDB from "./config/db";
-
-import AuthRoutes from "./routes/auth";
-import UserRoutes from "./routes/users";
-import ProfileRoutes from "./routes/profile";
-import PostsRoutes from "./routes/posts";
-
+import app from "./server";
 dotenv.config();
 
-// setup
-const app: Express = express();
 const PORT = parseInt(process.env.PORT as string) || 8080;
-
-// connect db
-connectDB();
-
-// MW
-app.use(helmet());
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(morgan("dev"));
-
-// ROUTES
-app.get("/", (_req, res) => res.send("API RUNNING"));
-app.use("/api/auth", AuthRoutes);
-app.use("/api/users", UserRoutes);
-app.use("/api/profile", ProfileRoutes);
-app.use("/api/posts", PostsRoutes);
 
 app.listen(PORT, () => {
   console.log(`=== SERVER STARTED ON http://localhost:${PORT} ===`);
