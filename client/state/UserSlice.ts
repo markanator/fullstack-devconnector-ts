8f6b371ea02d008a218cd70a85e5f8da66e63446
Mark Ambro <wowmarkambro@gmail.com>
2021-04-18 02:39:57 -0500

    fix build issue

--- client/state/UserSlice.ts
+++ client/state/UserSlice.ts
@@ -15,7 +15,7 @@ export const userSlice = createSlice({
 export const { hi } = userSlice.actions;
 
 // Other code such as selectors can use the imported `RootState` type
-export const selectUser = (state: RootState) => state.user;
+// export const selectUser = (state: RootState) => state.user;
 
 // export reducer for store
 export default userSlice.reducer;
